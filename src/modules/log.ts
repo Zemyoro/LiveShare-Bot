6d9a7de85272d95661213af639876862d5c07f0e
Zemyoro <85952603+Zemyoro@users.noreply.github.com>
2022-09-07 09:07:29 +0900

    Update log.ts

--- src/modules/log.ts
+++ src/modules/log.ts
@@ -1,12 +1,20 @@
 import fs from 'fs';
 
 const firstLine = 'Problems will be logged here';
+const clearedOn = () => {
+    const date = new Date();
+    return `Cleared on ${date.getFullYear()}/${date.getMonth()}/${date.getDay()} (${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}.${date.getMilliseconds()})`;
+}
 
 export default function(data: string) {
     console.log(data);
     if (!fs.existsSync('./log.txt')) fs.writeFileSync('./log.txt', firstLine);
-    let logFile: any = fs.readFileSync('./log.txt').toString();
+    let logFile: string | string[] = fs.readFileSync('./log.txt').toString();
     logFile = logFile.split('\n');
+
+    if (logFile.length > 200) logFile = [firstLine, clearedOn()];
+
     logFile.push(data);
+    logFile = logFile.join('\n');
     fs.writeFileSync('./log.txt', logFile);
 }
